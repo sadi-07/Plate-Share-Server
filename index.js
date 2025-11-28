10b5561097e81708f452218c56ef818fea9c15f3
sadi-07 <kmsadi2004@gmail.com>
2025-11-29 02:53:47 +0600

    GET POST Added

---
+++ index.js
@@ -0,0 +1,72 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+//plate-db
+//FXzKZSnZHxHEDWYs
+
+const uri = "mongodb+srv://plate-db:FXzKZSnZHxHEDWYs@cluster0.r81fqjh.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('plate-db')
+    const foodCollection = db.collection('foods')
+
+
+    // get method
+    app.get('/foods', async (req, res) => {
+
+        const result = await foodCollection.find().toArray()
+
+        res.send(result)
+    })
+
+    // post method
+    app.post('/foods', async (req, res) => {
+        const data = req.body;
+        const result = await foodCollection.insertOne(data)
+
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Plate Share listening on port ${port}`)
+})
