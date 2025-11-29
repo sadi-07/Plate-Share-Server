89aba0d295c53049bbf0f4f6e944484de5dc124a
sadi-07 <kmsadi2004@gmail.com>
2025-11-29 18:51:16 +0600

    env added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require ("dotenv").config();
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -7,10 +8,10 @@ const port = 3000
 app.use(cors())
 app.use(express.json())
 
-//plate-db
-//FXzKZSnZHxHEDWYs
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r81fqjh.mongodb.net/?appName=Cluster0`;
 
-const uri = "mongodb+srv://plate-db:FXzKZSnZHxHEDWYs@cluster0.r81fqjh.mongodb.net/?appName=Cluster0";
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
